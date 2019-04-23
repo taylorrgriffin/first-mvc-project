8aebe2a2dcf7d1804e2b0c70d032f567e152e078
alejandroidepaz <adepaz@cs.uoregon.edu>
2019-04-22 22:38:42 -0700

    Creation of home controller.

---
+++ project/Models/CalculatorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace project.Models
+{
+    public class CalculatorViewModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+
+        public string pageTitle { get; set; }
+    }
+}
